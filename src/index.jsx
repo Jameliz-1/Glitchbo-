d24423eb8d8f19039524f7519c894d2fc7aacd7c
Glitch (golden-good-eggplant) <none>
2024-09-14 17:57:52 +0000

    🚡💾 Checkpoint ./src/index.jsx:70189122/979

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.jsx";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { HelmetProvider } from 'react-helmet-async';
+import './index.css';
 
 /**
-* Root of react site 
-*
-* Imports Helmet provider for the page head
-* And App which defines the content and navigation
-*/
+ * Root of react site
+ *
+ * Imports Helmet provider for the page head
+ * And App which defines the content and navigation
+ */
 
-// Render the site https://reactjs.org/docs/react-dom.html#render
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
