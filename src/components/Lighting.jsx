58b9ba38c9d4d61685ccdab0f6517aa703610b68
Jameliz-1 <jamelizvip@yahoo.com>
2024-09-14 22:46:32 -0700

    Update Lighting.jsx
    Claude

--- src/components/Lighting.jsx
+++ src/components/Lighting.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 
 const Lighting = () => {
-
   return (
-
     <>
-
       <ambientLight intensity={0.5} />
-
       <pointLight position={[10, 10, 10]} intensity={1} castShadow />
-
       <spotLight
-
-        position={[0, 10, 0]}
-
+        position={[0, 15, 0]}
         angle={0.3}
-
         penumbra={1}
-
-        intensity={1}
-
+        intensity={1.5}
         castShadow
-
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
       />
-
     </>
-
   );
-
 };
 
 export default Lighting;
