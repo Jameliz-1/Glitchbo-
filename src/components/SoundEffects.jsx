e6058f8289f5d415d3606fce7c5dcc1b3c05f99b
Jameliz-1 <jamelizvip@yahoo.com>
2024-09-14 22:47:23 -0700

    Update SoundEffects.jsx
    Claude

--- src/components/SoundEffects.jsx
+++ src/components/SoundEffects.jsx
@@ -1,69 +1,35 @@
 import React, { useEffect, useRef } from 'react';
 
-const SoundEffects = ({ gameState }) => {
-
+const SoundEffects = ({ gameState, playPunchSound }) => {
   const backgroundMusicRef = useRef(new Audio('/sounds/background.mp3'));
-
-  const punchSoundRef = useRef(new Audio('/sounds/punch.mp3'));
-
   const victorySoundRef = useRef(new Audio('/sounds/victory.mp3'));
-
   const defeatSoundRef = useRef(new Audio('/sounds/defeat.mp3'));
 
   useEffect(() => {
-
     backgroundMusicRef.current.loop = true;
-
     if (gameState === 'fighting') {
-
       backgroundMusicRef.current.play();
-
     } else {
-
       backgroundMusicRef.current.pause();
-
       backgroundMusicRef.current.currentTime = 0;
-
     }
-
     return () => {
-
       backgroundMusicRef.current.pause();
-
       backgroundMusicRef.current.currentTime = 0;
-
     };
-
   }, [gameState]);
 
   useEffect(() => {
-
     if (gameState === 'gameOver') {
-
       if (Math.random() > 0.5) {
-
         victorySoundRef.current.play();
-
       } else {
-
         defeatSoundRef.current.play();
-
       }
-
     }
-
   }, [gameState]);
 
-  const playPunchSound = () => {
-
-    punchSoundRef.current.currentTime = 0;
-
-    punchSoundRef.current.play();
-
-  };
-
   return null;
-
 };
 
 export default SoundEffects;
